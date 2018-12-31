4641bc84516ec79777833d29b6cc5868377570a4
JanRou <janrou@gmail.com>
2018-12-31 15:01:15 +0100

    Played with selenium

--- NasdaqOmxScraperPoc/Program.cs
+++ NasdaqOmxScraperPoc/Program.cs
@@ -12,13 +12,16 @@ namespace NasdaqOmxScraperPoc {
     class Program {
         static void Main(string[] args) {
 
+            string url = "http://www.nasdaqomxnordic.com/aktier";
+            SeleniumPoc nasdaq = new SeleniumPoc(url);
+            nasdaq.GetNasdaqOmxStocklist();
+            /*
             string url = "http://www.nasdaqomxnordic.com/webproxy/DataFeedProxy.aspx?" +
                 "SubSystem=History&Action=GetChartData&inst.an=id%2Cnm%2Cfnm%2Cisin%2Ctp%2Cchp%2Cycp&FromDate=1986-01-01&ToDate=2018-12-07&" +
                 "json=true&showAdjusted=true&app=%2Faktier%2Fmicrosite-MicrositeChart-history&timezone=CET&DefaultDecimals=false&Instrument=CSE3331";
 
             // returnerer json til en kurve med dagspriser siden 1986 for AMBU-B. Aktier kan findes på aktie-siden ved at gemme tabellen med aktier
          
-
             // Create a request for the URL. 		
             WebRequest request = WebRequest.Create(url);
             // If required by the server, set the credentials.
@@ -31,15 +34,18 @@ namespace NasdaqOmxScraperPoc {
             Stream dataStream = response.GetResponseStream();
             // Open the stream using a StreamReader for easy access.
             StreamReader reader = new StreamReader(dataStream);
-            // Read the content.
-            string responseFromServer = reader.ReadToEnd();
+            // Store the content in file
+            using (System.IO.StreamWriter file =new System.IO.StreamWriter(@"C:\Temp\Test.txt")) {
+                file.Write(reader.ReadToEnd());
+            }
+            //string responseFromServer = reader.ReadToEnd();
             // Display the content.
-            Console.WriteLine(responseFromServer);
+            //Console.WriteLine(responseFromServer);
             // Cleanup the streams and the response.
             reader.Close();
             dataStream.Close();
             response.Close();
-
+            */
         }
     }
 }
---
+++ NasdaqOmxScraperPoc/SeleniumPoc.cs
@@ -0,0 +1,55 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using System.Collections.Generic;
+
+// Requires reference to WebDriver.Support.dll
+//using OpenQA.Selenium.Support.UI;
+
+namespace NasdaqOmxScraperPoc {
+
+    public class SeleniumPoc {
+
+        private readonly string _url;
+
+        public SeleniumPoc(string url) {
+            _url = url;
+        }
+
+        public void GetNasdaqOmxStocklist() {
+            using (IWebDriver driver = new FirefoxDriver()) {
+                driver.Navigate().GoToUrl(_url);
+
+                // Find searchSharesFormId
+                IWebElement searchSharesForm = driver.FindElement(By.Id("searchSharesFormId"));
+
+                IWebElement nordicSharesLabel = null;
+                IWebElement firstNorthSharesLabel = null;
+                ICollection<IWebElement> labels = searchSharesForm.FindElements(By.TagName("label"));
+                foreach (IWebElement label in labels) {
+                    if (label.GetAttribute("for") == "nordicShares") {
+                        nordicSharesLabel = label;
+                    }
+                    if (label.GetAttribute("for") == "firstNorth") {
+                        firstNorthSharesLabel = label;
+                    }
+                }
+                // <label for="nordicShares" class="ui-button ui-widget ui-state-default ui-button-text-only ui-corner-left ui-state-active" role="button" aria-disabled="false" aria-pressed="true">
+                //    <span class="ui-button-text">Main Market</span>
+                // </label>
+
+                // Get the resulting table of stocks id information
+                // <table id="searchSharesListTable"  ... >
+                //IWebElement searchSharesListTable = driver.FindElement(By.Id("searchSharesListTable"));
+                //searchSharesListTable.
+                if (nordicSharesLabel!= null) {
+                    // Write it to a file
+                    using (System.IO.StreamWriter file = new System.IO.StreamWriter(@"C:\Temp\SearchSharesListTable.txt")) {
+                        //file.Write(searchSharesListTable.Text);
+                        file.Write(nordicSharesLabel.Text);
+                    }
+                }
+
+            }
+        }
+    }
+}
