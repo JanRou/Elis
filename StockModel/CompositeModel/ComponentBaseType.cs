6fc6e04284e2b274be0abeac101ad3e1c680cf3e
Jan Rou <janrou@gmail.com>
2023-12-13 19:59:11 +0100

    Fjernet alle projekter, starter forfra

--- StockModel/CompositeModel/ComponentBaseType.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CompositeModel {
-
-    // The enum have to match all serializations like XML or JSON.
-    public enum ComponentBaseType {
-          Compo
-        , Leaf
-        , Error
-    }
-}
