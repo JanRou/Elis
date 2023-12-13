be8665a8f5028e9575be308c443429c6a6bd7933
Jan Rou <janrou@gmail.com>
2023-12-13 21:07:46 +0100

    Tilføjer første fluent migration

---
+++ ApplyMigrations/20180430_AddLogTable.cs
@@ -0,0 +1,21 @@
+﻿using FluentMigrator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplyMigrations {
+
+    [Migration(20180430121800)]
+    public class AddLogTable : Migration {
+        public override void Up() {
+            opret noget i databasen
+        }
+
+        public override void Down() {
+            throw new NotImplementedException();
+        }
+
+    }
+}
--- ApplyMigrations/Program.cs
+++ ApplyMigrations/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ApplyMigrations;
+using FluentMigrator.Runner;
+using Microsoft.Extensions.DependencyInjection;
+
+using (var serviceProvider = CreateServices()) {
+    using (var scope = serviceProvider.CreateScope()) {
+        // Put the database update into a scope to ensure
+        // that all resources will be disposed.
+        UpdateDatabase(scope.ServiceProvider);
+    }
+}
+
+static ServiceProvider CreateServices() {
+    return new ServiceCollection()
+        // Add common FluentMigrator services
+        .AddFluentMigratorCore()
+        .ConfigureRunner(rb => rb
+            // Add SQLite support to FluentMigrator
+            .AddPostgres11_0()
+            // Set the connection string
+            .WithGlobalConnectionString("Data Source=test.db")
+            // Define the assembly containing the migrations
+            .ScanIn(typeof(AddLogTable).Assembly).For.Migrations())
+        // Enable logging to console in the FluentMigrator way
+        .AddLogging(lb => lb.AddFluentMigratorConsole())
+        // Build the service provider
+        .BuildServiceProvider(false);
+}
+
+/// <summary>
+/// Update the database
+/// </summary>
+ static void UpdateDatabase(IServiceProvider serviceProvider) {
+    // Instantiate the runner
+    var runner = serviceProvider.GetRequiredService<IMigrationRunner>();
+
+    // Execute the migrations
+    runner.MigrateUp();
+}
