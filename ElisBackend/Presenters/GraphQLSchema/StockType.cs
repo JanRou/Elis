9ee2e2684cb89889e92bfbc847a01e3f855a5232
JanRou <janrou@gmail.com>
2024-07-17 13:03:55 +0200

    Testet og startet tilføjelse af nested CQL-typer

--- ElisBackend/Presenters/GraphQLSchema/StockType.cs
+++ ElisBackend/Presenters/GraphQLSchema/StockType.cs
@@ -8,8 +8,12 @@ namespace ElisBackend.Presenters.GraphQLSchema
             Description = "Basic stock information";
             Field(f => f.Name).Description("The name of the stock");
             Field(f => f.Isin).Description("The ISIN code for the stock");
-            Field( "exchange", f => f.Exchange.Name).Description("The exchange name for the stock");
-            Field( "currency", f => f.Currency.Code).Description("The currency code for the stock");
+            Field("exchange", f => f.Exchange.Name).Description("The exchange name for the stock");
+            Field("currency", f => f.Currency.Code).Description("The currency code for the stock");
+            // TODO Tilføj nested typer for børs (exchange) og valuta (currency)
+            //Field<ExchangeType>("exchange", f => f.Exchange.Name).Description("The exchange name for the stock");
+            //Field<CurrencyType>("currency", f => f.Currency).Description("The currency code for the stock");
+
             // TODO time series of prices
             //Field<ListGraphType<StockPriceSeriesType>>("priceseries")
             //    .Argument<NonNullGraphType<StringGraphType>>("periodeStart")
