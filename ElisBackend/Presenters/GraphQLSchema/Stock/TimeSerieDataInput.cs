217197415ebc79f1d249e55fc6b80c23c01d176d
JanRou <janrou@gmail.com>
2024-10-04 17:13:16 +0200

    Typo

--- ElisBackend/Presenters/GraphQLSchema/Stock/TimeSerieDataInput.cs
+++ ElisBackend/Presenters/GraphQLSchema/Stock/TimeSerieDataInput.cs
@@ -6,7 +6,7 @@ namespace ElisBackend.Presenters.GraphQLSchema.Stock {
         public TimeSerieDataInput() {
             Name = "TimeSerieDataInput";
             Field<NonNullGraphType<StringGraphType>>("date")
-                .Description("Date in UTC ISO 8601 format: '2024-07-24T00:00:00.000Z'");
+                .Description("Date in UTC ISO 8601 format: '2024-07-24T00:00:00.00000Z'");
             Field<NonNullGraphType<DecimalGraphType>>("price").Description("Price as decimal");
             Field<NonNullGraphType<DecimalGraphType>>("volume").Description("Volume as decimal");
         }
